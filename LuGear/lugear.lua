3b8954d82b911f240afe1acc842e0fe661f0b645
InnLumin <innlumin@example.com>
2026-02-22 01:53:07 -0500

    - Bumped version

--- LuGear/lugear.lua
+++ LuGear/lugear.lua
@@ -1,6 +1,6 @@
 addon.name = "LuGear"
 addon.author = "InnLumin"
-addon.version = "1.1.3"
+addon.version = "1.1.4"
 addon.desc = "A helper addon to help make, making luashitacast profiles easier."
 addon.link = "https://github.com/InnLumin/LuGear"
 
